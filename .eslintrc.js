2ec6c68e8abc31845c9089d23dd174b0126af041
Abelbait <95360969+Abelbait@users.noreply.github.com>
2022-01-23 12:03:53 +0800

    [NET-0031] Implement automated eslint (#44)
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * Testing Github Actions
    
    * implement eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+    ],
+    "rules": {
+    }
+}
