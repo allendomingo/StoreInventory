14b9bc4728984dc76cc74a5634ae445cc4ce1f13
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-01-23 22:06:10 +0800

    NET-0041: Transfer supplier and contact swagger documentation to respective files from suppliers router

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Contact:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The contact person's name.
+ *         numbers:
+ *           type: Array<string>
+ *           description: The contact person's phone number/s.
+ *         emails:
+ *           type: Array<string>
+ *           description: The contact person's email/s.
+ *         contactType:
+ *           type: string
+ *           description: Whether the contact is a customer or a supplier
+ */
+
 const contactSchema = new Schema({
   name: {
     type: String,
--- models/supplier.js
+++ models/supplier.js
@@ -1,6 +1,31 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Supplier:
+ *       allOf:
+ *         - type: object
+ *           properties:
+ *             name:
+ *               type: string
+ *               description: The supplier's name.
+ *             address:
+ *               type: Array<string>
+ *               description: The supplier's address/es.
+ *             contacts:
+ *               type: Contact
+ *               description: The supplier's contact information
+ *             brands:
+ *               type: [string]
+ *               description: The supplier's address/es.
+ *             notes:
+ *               type: string
+ *               description: Additional notes.
+ */
+
 const supplierSchema = new Schema({
   name: {
     type: String,
