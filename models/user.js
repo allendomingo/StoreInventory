858868d3a6749f68d85f7cf779c30d068e37f07c
Raymund Rafael <raymund.adrian.rafael@gmail.com>
2022-01-29 17:22:28 +0800

    [NET-0003] Add user model file and swagger documentation

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const { ROLES } = require('../constants/roles.js')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const userDefinition = {
+	$firstName: 'Jonel',
+	$lastName: 'Babao',
+	$email: 'mvp@email.com',
+  $role: 'admin'
+};
+
+const User = new Schema({
+    firstName: {
+        type: String,
+        default: ''
+    },
+    lastName: {
+        type: String,
+        default: ''
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    role: {
+      type: String,
+      enum: Object.values(ROLES),
+      required: true,
+      default: 'user'
+    }
+})
+
+User.plugin(passportLocalMongoose)
+
+module.exports = {
+	model: User,
+	definition: userDefinition,
+};
