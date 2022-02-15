1a8a646c694a1831b562319ffa690b9a753dda56
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-02-15 23:00:50 +0800

    NET-0009: Change expected value type of contacts in supplier definition

--- models/supplier.js
+++ models/supplier.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const supplierDefinition = {
 	$name: 'Ben\'s Supplies',
 	$address: '#1 Road St., City, Province, Philippines 0000',
-	contacts: [{ $ref: '#/definitions/Contact' }],
+	contacts: ['Contact ObjectId'],
 	brands: ['BenQ', 'Corsair'],
 	notes: 'Friday specials',
 };
