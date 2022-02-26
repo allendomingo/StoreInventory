880094fb71616ca3b4bd29b843260fc5d49cb7fa
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-02-27 02:07:10 +0800

    NET-0062: Fix lint issues in files under controller folders

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,29 +1,29 @@
 const Inventory = require('../models/inventory').model;
 
-exports.getItems = function(params) {
-	return Inventory.find(params);
+exports.getItems = function (params) {
+  return Inventory.find(params);
 };
 
-exports.getItem = function(itemId) {
-	return Inventory.findById(itemId);
+exports.getItem = function (itemId) {
+  return Inventory.findById(itemId);
 };
 
-exports.createItem = function(item) {
-	return Inventory.create(item);
+exports.createItem = function (item) {
+  return Inventory.create(item);
 };
 
-exports.deleteItems = function() {
-	return Inventory.deleteMany({});
+exports.deleteItems = function () {
+  return Inventory.deleteMany({});
 };
 
-exports.updateItem = function(itemId, updateParams) {
-	return Inventory.findByIdAndUpdate(
-		itemId,
-		{ $set: updateParams },
-		{ new: true },
-	);
+exports.updateItem = function (itemId, updateParams) {
+  return Inventory.findByIdAndUpdate(
+    itemId,
+    { $set: updateParams },
+    { new: true },
+  );
 };
 
-exports.deleteItem = function(itemId) {
-	return Inventory.findByIdAndRemove(itemId);
+exports.deleteItem = function (itemId) {
+  return Inventory.findByIdAndRemove(itemId);
 };
