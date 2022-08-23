b4d93b4d3ed3f3455bd5380e6d0c31ae5534c0ec
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-08-24 00:54:21 +0800

    Test Lint Github Actions

--- inventory-web-app/pages/index.js
+++ inventory-web-app/pages/index.js
@@ -20,6 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
