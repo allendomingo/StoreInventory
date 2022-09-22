214bf2e851c0d194f384796f0794f22bcef650d3
allendomingo <allen.domingo@eee.upd.edu.ph>
2022-09-23 00:43:47 +0800

    NET-0100: Arrange stories

--- stockpile/src/stories/examples/Page.stories.jsx
+++ stockpile/src/stories/examples/Page.stories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { within, userEvent } from '@storybook/testing-library';
 
-import { Page } from '../components/examples';
+import { Page } from '../../components/examples';
 
 export default {
   title: 'Example/Page',
