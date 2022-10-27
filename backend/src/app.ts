61d6e67f45935ddecf03c99c04ba61634c71de04
gachagas <gerlito.chagas@gmail.com>
2022-10-27 21:56:32 +0800

    add eslint and prettier

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`[server]: Server is now running at https://localhost:${port}`);
-});
+  console.log(`[server]: Server is now running at https://localhost:${port}`)
+})
