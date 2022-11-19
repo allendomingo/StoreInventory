2fa4cdbc5eedf0aa21e7ab384ed67e7a7775c767
gachagas <gerlito.chagas@gmail.com>
2022-11-19 10:08:13 +0800

    add contact model

---
+++ backend/src/models/contact.ts
@@ -0,0 +1,50 @@
+import { model, Schema } from 'mongoose'
+
+enum ContactType {
+  supplier = 'Supplier',
+  customer = 'Customer'
+}
+
+const contactDefinition = {
+  $name: 'Juan dela Cruz',
+  numbers: ['0900XXXXXXX'],
+  emails: ['juandelacruz@email.com'],
+  $contactType: [ContactType.customer]
+}
+
+interface IContact {
+  name: string
+  numbers: [string]
+  emails: [string]
+  contactType: string
+}
+
+const contactSchema = new Schema<IContact>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    numbers: {
+      type: [String]
+    },
+    emails: {
+      type: [String]
+    },
+    contactType: {
+      type: String,
+      enum: Object.values(ContactType),
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Contact = model('Contact', contactSchema)
+
+export { Contact, contactDefinition }
+
+export default Contact
